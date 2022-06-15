96da6fc2b898478890e625fb85ecb0ff6a9884b6
ichikura <ichikura@jsk.imi.i.u-tokyo.ac.jp>
2022-06-15 18:14:46 +0900

    add unitree reaction

---
+++ jsk_unitree_robot/jsk_unitree_startup/scripts/emotion_talker.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+from speech_recognition_msgs.msg import SpeechRecognitionCandidates
+from jsk_recognition_msgs.msg import PeoplePoseArray
+import time
+import message_filters
+
+from threading import Lock
+
+
+class testNode():
+    def __init__(self):
+        # Publisher
+        self.pub = rospy.Publisher('/emotion_chatter', String, queue_size=1)
+
+        # Subscriber
+        self.sub1 = rospy.Subscriber('/Tablet/voice', SpeechRecognitionCandidates, self.speech_callback)
+        self.sub2 = rospy.Subscriber('/people_pose', PeoplePoseArray, callback=self.callback)
+        self.duration_time = rospy.Duration(30)
+        self.prev_pose_detected_time = rospy.Time.now() - self.duration_time
+
+        self.lock = Lock()
+
+        queue_size = 10
+        fps = 100.
+        delay = 1 / fps * 0.5
+
+    def callback(self, msg):
+        if (rospy.Time.now() - self.prev_pose_detected_time) < self.duration_time:
+            return
+        poses = msg.poses
+        if len(poses) >= 1:
+            item = poses[0].limb_names
+            scores = poses[0].scores
+            target_limbs = [
+                "left_eye", "nose", "right_eye", "right_ear",
+                "left_ear"]
+            if all([name in item for name in target_limbs]):
+                word = "happy"
+                with self.lock:
+                    print("publish {}".format(word))
+                    self.publish(word)
+                    self.prev_pose_detected_time = rospy.Time.now()
+                    rospy.sleep(5.0)
+
+    def publish(self, data):
+        self.pub.publish(data)
+
+    def speech_callback(self, msg):
+        print(type(msg))
+        word = msg.transcript[0]
+        if word in ["こんにちは",
+                    "やっほー",
+                    "こんばんは",
+                    "おはよう",
+                    "おはようございます"]:
+            message = "happy"
+        elif word in ["可愛いね",
+                      "可愛い",
+                      "かわいいね",
+                      "かわいい"]:
+            message = "joy"
+        elif word in ["散歩に行こう",
+                      "散歩",
+                      "行こう",
+                      "いこう"]:
+            message = "affirmation"
+        elif word in ["今日は行けないよ",
+                      "いけないよ",
+                      "行けないよ"]:
+            message = "negation"
+        elif word in ["大好きだよ",
+                      "好き",
+                      "すき"]:
+            message = "love"
+        elif word in ["あっち行って",
+                      "さようなら"]:
+            message = "scared"
+        else:
+            message = word
+        print(message)
+        with self.lock:
+            print("self.publish(message) {}".format(message))
+            self.publish(message)
+            rospy.sleep(5.0)
+
+
+if __name__ == '__main__':
+    rospy.init_node('speech_to_string')
+
+    time.sleep(3.0)
+    node = testNode()
+
+    while not rospy.is_shutdown():
+        rospy.sleep(0.1)
+        
